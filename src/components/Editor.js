a32ac9e213cc749c3de31bf1500563bc2bbc457c
zurajacoco <zurajacoco@gmail.com>
2018-06-14 17:16:09 +0900

    feat(components/Editor): Tabの制御用にonKeyDownを追加

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default (props) => {
   return (
     <div>
-      <textarea onChange={props.onChange} />
+      <textarea onChange={props.onChange} onKeyDown={props.onKeyDown} />
     </div>
     );
 };
