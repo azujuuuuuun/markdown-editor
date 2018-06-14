5c716b9afe3bd1eedb22bb3a4effb580b3507d7e
zurajacoco <zurajacoco@gmail.com>
2018-06-14 17:13:36 +0900

    feat(containers/EditorContainer): handleKeydownでTab入力の挙動を制御

--- src/containers/EditorContainer.js
+++ src/containers/EditorContainer.js
@@ -6,9 +6,26 @@ export default class EditorContainer extends React.Component {
     super(props);
   }
 
+  handleKeyDown = (e) => {
+    if (e.key === 'Tab') {
+      if (e.preventDefault) {
+        e.preventDefault();
+      }
+
+      const elem = e.target;
+      const start = elem.selectionStart;
+      const end = elem.selectionEnd;
+      const value = elem.value;
+
+      elem.value = value.substring(0, start) + '\t' + value.substring(end);
+      elem.selectionStart = elem.selectionEnd = start + 1;
+      this.props.onChange(e);
+    }
+  }
+
   render() {
     return (
-        <Editor onChange={this.props.onChange} />
+      <Editor onChange={this.props.onChange} onKeyDown={this.handleKeyDown} />
       );
   }
 }
