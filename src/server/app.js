4c20cf4dd71a7f1d197cfb817fda01cc6b09051a
azujuuuuuun <35828118+azujuuuuuun@users.noreply.github.com>
2018-07-19 16:50:35 +0900

    feat(server/app): POST /signupの処理を追加

--- src/server/app.js
+++ src/server/app.js
@@ -1,10 +1,35 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const db = require('../../db/models');
+const user = db.sequelize.import('../../db/models/user');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.use(express.static('public'));
 app.use('/dist', express.static('dist'));
 app.use('/public/css', express.static('public/css'));
+
+app.post('/signup', (req, res) => {
+  const {
+    userName,
+    email,
+    password,
+  } = req.body;
+  user.create({
+    userName: userName,
+    email: email,
+    password: password,
+  })
+    .then(() => {
+      res.status(200).send();
+    })
+    .catch((err) => {
+      res.status(400).send(err);
+    });
+});
 
 app.listen(3000, () => {
   console.log('markdown-editor listening on port 3000!');
