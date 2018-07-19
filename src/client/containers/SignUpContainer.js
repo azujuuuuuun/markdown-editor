8dfc4ea75dbb92a2d9502296404bebfacbcabf21
zurajacoco <zurajacoco@gmail.com>
2018-07-19 16:38:12 +0900

    feat(containers/SignUpContainer): stateの設定とフォームのonChangeの追加

--- src/client/containers/SignUpContainer.js
+++ src/client/containers/SignUpContainer.js
@@ -4,9 +4,43 @@ import SignUp from '../components/SignUp';
 export default class SignUpContainer extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      userName: '',
+      email: '',
+      password: '',
+      auth: false,
+      errorMessage: '',
+    };
+  }
+
+  handleUserName = (e) => {
+    this.setState({
+      userName: e.target.value,
+    });
+  }
+
+  handleEmail = (e) => {
+    this.setState({
+      email: e.target.value,
+    });
+  }
+
+  handlePassword = (e) => {
+    this.setState({
+      password: e.target.value,
+    });
+  }
+
   }
 
   render() {
+    const {
+      userName,
+      email,
+      password,
+      auth,
+      errorMessage,
+    } = this.state;
     return (
       <SignUp />
       );
