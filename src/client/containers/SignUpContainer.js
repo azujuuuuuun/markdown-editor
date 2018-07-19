b1739c6f9337a956835711957601b3f291b9b09d
zurajacoco <zurajacoco@gmail.com>
2018-07-19 13:44:18 +0900

    feat(SignUp): SIgnUpフォームとCSSの追加

---
+++ src/client/containers/SignUpContainer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import SignUp from '../components/SignUp';
+
+export default class SignUpContainer extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <SignUp />
+      );
+  }
+}
