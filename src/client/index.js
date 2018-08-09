e55477a3b6ca3612304b75709d6d3031075dc2da
zurajacoco <zurajacoco@gmail.com>
2018-08-09 17:25:37 +0900

    refactor(index): 認証とルーティングを挟むように変更

--- src/client/index.js
+++ src/client/index.js
@@ -2,24 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {createStore} from 'redux';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 import rootReducer from './reducers';
-import App from './components/App';
-import TopPage from './components/TopPage';
-import SignUpPage from './components/SignUpPage';
-import LoginPage from './components/LoginPage';
+import Auth from './containers/AuthContainer';
+import Routing from './containers/RoutingContainer';
 
 const store = createStore(rootReducer);
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <div>
-        <Route exact path='/' component={TopPage} />
-        <Route path='/signup' component={SignUpPage} />
-        <Route path='/login' component={LoginPage} />
-        <Route path='/app' component={App} />
-      </div>
+      <Auth>
+        <Routing />
+      </Auth>
     </BrowserRouter>
   </Provider>,
   document.getElementById('root')
