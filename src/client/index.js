2a3dd18bc702e0dc3767708c7567e3ad2a50c52c
azujuuuuuun <35828118+azujuuuuuun@users.noreply.github.com>
2018-08-09 14:06:31 +0900

    feat(index): グローバルstateをreduxで管理するように変更

--- src/client/index.js
+++ src/client/index.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import {BrowserRouter, Route} from 'react-router-dom';
+import rootReducer from './reducers';
 import App from './components/App';
 import TopPage from './components/TopPage';
 import SignUpPage from './components/SignUpPage';
 import LoginPage from './components/LoginPage';
-import {BrowserRouter, Route} from 'react-router-dom';
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(
-  <BrowserRouter>
-    <div>
-      <Route exact path='/' component={TopPage} />
-      <Route path='/signup' component={SignUpPage} />
-      <Route path='/login' component={LoginPage} />
-      <Route path='/app' component={App} />
-    </div>
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <div>
+        <Route exact path='/' component={TopPage} />
+        <Route path='/signup' component={SignUpPage} />
+        <Route path='/login' component={LoginPage} />
+        <Route path='/app' component={App} />
+      </div>
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
